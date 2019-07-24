6c1f7727c21feced9c9a4e5fd6758662f18389f5
Ashim Sedhain <asimsedhain33@gmail.com>
2019-07-24 11:19:21 -0500

    Added the roomcheck.js file

--- roomCheck.js
+++ roomCheck.js
@@ -1,3 +1,27 @@
 class roomCheck{
+	let i = `I went by | COB | 207 | 20/05/2019 | Last Checked: N/A | and did a room check of its contents: 
+
+	Projector – The projector image is bright and aligned 
+	Projector Model: (2) NEC NP-P502WL-2 | Inv. #: None | S/N: 8300225TB and 7X00237RR, respectively | Lamp Hours: 1010 and 1007 Hours | Air Filter Hours: Self-Cleaning | Lamp Hours < 4000, Lamp replacement is not currently warranted 
 	
+	Computer- 
+	Computer Model: Dell OptiPlex 7040 | Inv. #: White: 027126 | S/N: 4RMSND2 | Name: CL-COB207 | OS: Win10(x64) Version 1709 | 
+	
+	System- 
+	Components: 
+	Crestron- 
+	○ TSW-760-TTK-B-S (6507727) Touchpanel 
+	○ DMPS3-300-C (Inv. #: None)(S/N: 1746JBH07231) 
+	Cisco Catalyst 3560-CX Series Switch(Inv. #: None)(Mfg. Date: 20180620), ELMO TT-12iD Document Camera(Inv. #: None)(S/N: 1405516), Sharp PN-LE601 TV (S/N: 803611497), Shure SM58 Handheld mic, Shure SLX1 Lapel Mic, Shure SLX4 Wireless Mic Rec., SerialComm Serial Port Isolator | Sources: Computer, Laptop VGA, Laptop HDMI, Doc Cam | 
+	
+	Functionality- 
+	• All of the sources are working correctly and I was able to view and hear a YouTube video on the Computer source 
+	• I cleaned the station and made sure it was organized 
+	• The mics do not function 
+	
+	Updating the Combined Room Check Excel Spreadsheet on Teams 
+	
+	Waiting to resolve the incident until the issue with the mics is fixed. 
+	
+	Ashim S`
 }
