31415de7abc24f23950736456f6beb9b2cc847cd
Ashim Sedhain <asimsedhain33@gmail.com>
2019-11-02 05:12:36 -0500

    changed session param

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ app.use(cors());
 app.use(session({
 	name: 'session-id',
 	secret: process.env.SESSIONSECRET,
-	saveUninitialized: true,
-	resave: false,
+	saveUninitialized: false,
+	resave: true,
 	store: new mongoStore({ mongooseConnection: mongoose.connection }),
 	// cookie: { expires : new Date(Date.now() + 1800000) }
 }));
