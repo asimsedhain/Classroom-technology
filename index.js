996961ec85599522fd25a394d50714d52b46efd4
Ashim Sedhain <asimsedhain33@gmail.com>
2019-11-02 05:10:46 -0500

    changed session resave param to false

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(session({
 	name: 'session-id',
 	secret: process.env.SESSIONSECRET,
 	saveUninitialized: true,
-	resave: true,
+	resave: false,
 	store: new mongoStore({ mongooseConnection: mongoose.connection }),
 	// cookie: { expires : new Date(Date.now() + 1800000) }
 }));
